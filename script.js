a3a06f5733d802b84657febec362883d91572750
FloridaLangara <fjoca00@mylangara.ca>
2022-06-20 16:53:09 -0700

    Fix: Create SPA

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Router.init("main-area", [
+    new Page("#home", 'app/Home.html'),
+    new Page("#about", 'app/About.html'),
+    new Page("#profile", 'app/Profile.html'),
+    new Page("#nannyprofilelist", 'app/NannyProfileList.html'),
+    new Page("#signup", 'app/SignUp.html'),
+    new Page("#login", 'app/Login.html'),
+    new Page("#parentlist", 'app/ParentProfileList.html'),
+    new Page("#usersettings", 'app/UserSettings.html'),
+]);
