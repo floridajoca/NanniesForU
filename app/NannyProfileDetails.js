cd8b2f0d3bfeea509d58c56bc382ed316ea41b06
Bihara Singh <rbihara100@gmail.com>
2022-06-21 21:35:09 -0700

    Fix: Nanny Profile page and About page images

--- app/NannyProfileDetails.js
+++ app/NannyProfileDetails.js
@@ -10,33 +10,30 @@ const user_array = [];
 getDocs(colRef)
     .then((snapshot) => {
 
-        let user_array = [];
+        let user_array1 = [];
         //console.log(snapshot.docs);
 
         snapshot.docs.forEach((doc) => {
             user_array.push({...doc.data(), id: doc.id });
         })
         console.log(user_array);
+
     })
-    // .catch(err) {
-    //     console.log(err.message);
-    // }
+
+// .catch(err) {
+//     console.log(err.message);
+// }
 
 
 
 
-// nannyName.innerHTML = "Jess"
-// nannyLocation.innerHTML = 'Ontario';
-// scheduleOutput.innerHTML = 'MTWTFSS';
-// nannyRate.innerHTML = '35$ per hour';
-// startDateOutput.innerHTML = '22-June-2022';
-// endDateOutput.innerHTML = '31-Dec-2022';
-// descriptionOutput.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam deleniti cupiditate vero! Natus consequatur magnam ex autem illo neque quam!';
+
 
 let currentIndex = 0;
 
 next.addEventListener('click', () => {
-    alert("yes");
+
+
     if (currentIndex < user_array.length - 1) {
 
         currentIndex = currentIndex + 1;
@@ -60,7 +57,7 @@ prev.addEventListener('click', () => {
         currentIndex = user_array.length - 1;
     }
     nannyName.innerHTML = user_array[currentIndex].first_name;
-    nannyLocation.innerHTML = user_array[currentIndex].city
+    nannyLocation.innerHTML = user_array[currentIndex].city;
 
     nannyRate.innerHTML = user_array[currentIndex].pay_rate + ' per hour';
 
