820ddb577238a843f5b0e2b7af1eb9c748386655
FloridaLangara <fjoca00@mylangara.ca>
2022-06-28 16:18:13 -0700

    Fix: Reusable functions to check validation of email and password.

---
+++ app/utils/password.js
@@ -0,0 +1,6 @@
+export const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+}
+
+//Reference: https://www.javascripttutorial.net/javascript-dom/javascript-form-validation/
