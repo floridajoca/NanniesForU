3ebbf017130679d7f930d08eb12674f7ea5bcfa8
FloridaLangara <fjoca00@mylangara.ca>
2022-07-19 23:05:43 -0700

    Fix: Nanny Profile details refactor code

--- app/NannyProfileDetails.js
+++ app/NannyProfileDetails.js
@@ -1,100 +1,45 @@
 'use strict';
 import { db } from "../firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
-const colRef = collection(db, 'user');
-
-const user_array = [];
-getDocs(colRef)
-    .then((snapshot) => {
-
-        let user_array1 = [];
-        //console.log(snapshot.docs);
-
-        snapshot.docs.forEach((doc) => {
-            user_array.push({...doc.data(), id: doc.id });
-        })
-        console.log(user_array);
-
-    })
+import { collection,getDocs, getDoc, doc, query } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
 
 
-let currentIndex = 0;
+let  NannyProfileDetails = {};
 
-next.addEventListener('click', () => {
-
-
-    if (currentIndex < user_array.length - 1) {
-
-        currentIndex = currentIndex + 1;
-
-    } else {
-        currentIndex = 0;
-    }
-    fetch_data();
+window.onload = async function getNannyDetails(nannyId) {
+    await getDoc(doc(db, "user", "BYtcnnXnKKpMZ4dPMZpE")).then((doc) => {
+            NannyProfileDetails = doc.data();
+            console.log("her" + NannyProfileDetails)
+    });
 
 
-})
+    const nannyExperience = query(collection(db, "nanny"));
+    await getDocs(nannyExperience).then((snapshot) => {
+        snapshot.forEach((doc) => {
+            if(doc.data().nanny_id == "BYtcnnXnKKpMZ4dPMZpE") {
+                NannyProfileDetails = {
+                    ...NannyProfileDetails,
+                    ...doc.data(),
+                }
+            }
+        });
+    });
 
-prev.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex = currentIndex - 1;
-    } else {
-        currentIndex = user_array.length - 1;
-    }
-    fetch_data();
-
-
-})
-
-// ---------------------- Color ---------------
-
-function fetch_data() {
-    reset_color();
-    nannyName.innerHTML = user_array[currentIndex].full_name;
-    nannyLocation.innerHTML = user_array[currentIndex].city;
-    nannyRate.innerHTML = user_array[currentIndex].pay_rate + ' $ per hour';
-    nannyDescription.innerHTML = user_array[currentIndex].description;
-    payRate.value = user_array[currentIndex].pay_rate;
-
-    // tax_5.value = "";
-    // tax_7.value = "";
-    total.value = "";
-    duration.value = "";
-    let calendar_days = user_array[currentIndex].schedule;
-    console.log(calendar_days);
-    if (calendar_days != null) {
-        for (let day of calendar_days) {
-            console.log(day);
-            let day_id = document.getElementById("" + day.toLowerCase() + "");
-            console.log(day_id);
-            day_id.style.backgroundColor = "rgba(44, 171, 128, 1)";
-            day_id.style.color = "rgba(255, 255, 255, 1)";
-        }
-    }
-
+    renderNannyDetails(NannyProfileDetails);
+    console.log(NannyProfileDetails)
 }
 
-function reset_color() {
+function renderNannyDetails() {
+    const nannyName = document.querySelector("#nanny-name");
+    const nannyLocation = document.querySelector("#nanny-location");
+    const nannyDescription = document.querySelector("#nanny-description");
+    const nannyRate = document.querySelector("#nanny-rate");
+    const nannyAvailability =  document.querySelector("#nanny-availability");
+    const nannyReviews =  document.querySelector("#nanny-reviews");
 
-    var ul = document.getElementById("scheduleOutput");
-    var listItems = ul.getElementsByTagName("li");
-    for (let li of listItems) {
-        li.style.backgroundColor = "rgba(224, 224, 224, 1)";
-        li.style.color = "rgba(0, 0, 0, 1)";
-    }
+        nannyName.innerHTML += NannyProfileDetails.full_name;
+        nannyLocation.innerHTML += NannyProfileDetails.city;
+        nannyDescription.innerHTML += NannyProfileDetails.description ? NannyProfileDetails.description : "";
+        nannyRate.innerHTML += NannyProfileDetails.payrate;
+        nannyAvailability.innerHTML += NannyProfileDetails.schedule;
+        nannyReviews.innerHTML += NannyProfileDetails.ratings;
 }
-// -------------------- Rate Calculation -------------------
-
-calculate.addEventListener('click', () => {
-
-    const tx5 = duration.value * user_array[currentIndex].pay_rate * 5 / 100;
-    const tx7 = duration.value * user_array[currentIndex].pay_rate * 7 / 100;
-    const ttl = duration.value * user_array[currentIndex].pay_rate;
-
-    // tax_5.value = tx5;
-    // tax_7.value = tx7;
-    total.value = (ttl + tx5 + tx7).toFixed(2);
-
-    event.preventDefault();
-
-})
