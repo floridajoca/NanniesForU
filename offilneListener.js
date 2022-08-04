c92d8e2122511c2e8598d52c43ef6986f5e27999
FloridaLangara <fjoca00@mylangara.ca>
2022-08-04 15:07:00 -0700

    Offline style fixed

--- offilneListener.js
+++ offilneListener.js
@@ -10,12 +10,9 @@ function updateOnlineStatus() {
         main.innerHTML = "";
         const p = document.createElement('p');
         const div = document.createElement('div');
-        var img = document.createElement("img");
-        img.src = "app/img/logo_proposal_final.png";
-        div.append(img);
         div.classList.add('offline-wrapper');
         p.classList.add('offline-message');
-        p.innerText = "Sorry, No nannies for you! You're offline. Please reconnect and refresh the page!"
+        p.innerText = "Sorry, you lost connection. Please reconnect and refresh the page!"
         div.appendChild(p)
         main.appendChild(div)
         main.style.cssText = 'display: block';
